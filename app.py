8512ee38324e466272cdf9a453a05635b2b773e0
Красов Павел <pavel.a.krasov@gmail.com>
2024-09-07 14:26:01 +0700

    Вывод страницы первой лабораторной

--- app.py
+++ app.py
@@ -3,4 +3,23 @@ app = Flask(__name__)
 
 @app.route("/")
 def start():
-    return "web-сервер на flask"
+    return """
+<!doctype html>
+<html>
+    <head>
+        <title>Красов Павел Андреевич, лабораторная 1</title>
+    </head>
+    <body>
+        <header>
+            НГТУ, ФБ, лабораторная работа 1
+        </header>
+
+        <h1>web-сервер на flask</h1>
+
+        <footer>
+            &copy; Красов Павел, ФБИ-23, 3 курс, 2024
+        </footer>
+    </body>
+</html>
+"""
+
