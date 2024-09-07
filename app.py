9a6ac4d286f8a1c11973dc3d66bca88d52b76a89
Красов Павел <pavel.a.krasov@gmail.com>
2024-09-07 15:06:43 +0700

    Меню

--- app.py
+++ app.py
@@ -2,7 +2,35 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
+@app.route("/index")
 def start():
+    return """
+<!doctype html>
+<html>
+    <head>
+        <title>Красов Павел Андреевич, лабораторная 1</title>
+    </head>
+    <body>
+        <header>
+            НГТУ, ФБ, WEB-программирование, часть 2
+        </header>
+
+        <h1>web-сервер на flask</h1>
+
+        <h2>Меню</h2>
+        <ul>
+        <li><a href = "/lab1">Первая лабораторная</a></li>
+        </ul>
+
+        <footer>
+            &copy; Красов Павел, ФБИ-23, 3 курс, 2024
+        </footer>
+    </body>
+</html>
+"""
+
+@app.route("/lab1")
+def lab1():
     return """
 <!doctype html>
 <html>
@@ -15,6 +43,13 @@ def start():
         </header>
 
         <h1>web-сервер на flask</h1>
+
+        <p>Flask — фреймворк для создания веб-приложений на языке
+        программирования Python, использующий набор инструментов
+        Werkzeug, а также шаблонизатор Jinja2. Относится к категории так
+        называемых микрофреймворков — минималистичных каркасов
+        веб-приложений, сознательно предоставляющих лишь самые базовые возможности.
+        </p>
 
         <footer>
             &copy; Красов Павел, ФБИ-23, 3 курс, 2024
