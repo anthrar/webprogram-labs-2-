be9897faddc54332db8b60a56955b7815c901697
Красов Павел <pavel.a.krasov@gmail.com>
2024-09-17 17:10:45 +0700

    Статистические файлы

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, redirect
+from flask import Flask, redirect, url_for
 app = Flask(__name__)
 
 @app.route("/")
@@ -61,4 +61,21 @@ def lab1():
     </body>
 </html>
 """
+
+@app.route("/lab1/oak")
+def oak():
+    return '''
+<!doctype html>
+<html>
+    <head>
+        <link rel="stylesheet" type="text/css" href="''' + url_for('static', filename = 'lab1.css') + ''' " />
+    </head>
+    <body>
+        <div class="oak">
+            <h1>Дуб</h1>
+            <img src="''' + url_for('static', filename = 'oak.jpg') + ''' ">
+        </div>
+    </body>
+</html>
+'''
 
