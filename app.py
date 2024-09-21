a1e9a9ff675a58095beb6a3277fb4c6eaac0749d
Красов Павел <pavel.a.krasov@gmail.com>
2024-09-21 15:03:02 +0700

    Цикл в шаблоне

--- app.py
+++ app.py
@@ -213,4 +213,11 @@ def example():
     number = '2'
     group = 'ФБИ-23'
     course = '3'
-    return render_template('example.html', name=name,number=number, group=group, course=course)
+    fruits = [
+        {'name': 'манго', 'price': 100},
+        {'name': 'арбузы', 'price': 120},
+        {'name': 'бананы', 'price': 140},
+        {'name': 'яблоки', 'price': 80}, 
+        {'name': 'персики', 'price': 96}
+    ]
+    return render_template('example.html', name=name,number=number, group=group, course=course, fruits=fruits)
