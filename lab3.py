35ae7017dc2feb79229ad41b70c02941dab413b8
Красов Павел <pavel.a.krasov@gmail.com>
2024-10-16 15:44:32 +0700

    Заказ напитка

--- lab3.py
+++ lab3.py
@@ -41,3 +41,23 @@ def form1():
 @lab3.route('/lab3/order')
 def order():
     return render_template('lab3/order.html')
+
+@lab3.route('/lab3/pay')
+def pay():
+    price = 0
+    drink = request.args.get('drink')
+    #Кофе стоит 120 рублей, чёрный чай - 80 рублей, зелёный - 70 рублей
+    if drink == 'coffee':
+        price = 120
+    elif drink == 'black-tea':
+        price = 80
+    else:
+        price = 70
+    
+    #Добавка молока удорожает напиток на 30 рублей, сахара - на 10
+    if request.args.get('milk') == 'on':
+        price += 30
+    if request.args.get('sugar') == 'on':
+        price += 10
+    
+    return render_template('lab3/pay.html', price=price)
