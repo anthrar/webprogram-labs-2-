b704307a164dbb9be3a89eb1478dea283cb2d715
Красов Павел <pavel.a.krasov@gmail.com>
2024-10-18 19:01:46 +0700

    Железнодорожный билет

--- lab3.py
+++ lab3.py
@@ -61,3 +61,80 @@ def pay():
         price += 10
     
     return render_template('lab3/pay.html', price=price)
+
+@lab3.route('/lab3/settings')
+def settings():
+    color = request.args.get('color')
+    if color:
+        resp = make_response(redirect('/lab3/settings.html'))
+        resp.set_cookie('color', color)
+        return resp
+
+    color = request.cookies.get('color')
+    resp = make_response(render_template('lab3/settings.html', color=color))
+    return resp
+
+@lab3.route('/lab3/form_rjd')
+def form_rjd():
+    errors = {}
+    user = request.args.get ('user')
+    if user == '':
+        errors['user'] = 'Заполните поле!'
+    age = request.args.get ('age')
+    if age == '':
+        errors['age'] = 'Заполните поле!'
+    place = request.args.get ('place')
+    if place == '':
+        errors['place'] = 'Заполните поле!'
+    departure = request.args.get ('departure')
+    if departure == '':
+        errors['departure'] = 'Заполните поле!'
+    arrival = request.args.get ('arrival')
+    if arrival == '':
+        errors['arrival'] = 'Заполните поле!'
+    date = request.args.get ('date')
+    if date == '':
+        errors['date'] = 'Заполните поле!'
+    return render_template('lab3/form_rjd.html', user=user, age=age, place=place, 
+                           departure=departure, arrival=arrival, date=date, errors=errors)
+
+@lab3.route('/lab3/ticket')
+def ticket():
+    user = request.args.get('user')
+    departure = request.args.get('departure')
+    arrival = request.args.get('arrival')
+    date = request.args.get('date')
+    underwear = request.args.get('underwear')
+    luggage = request.args.get('luggage')
+    belay = request.args.get('belay')
+    price = 0
+    age = int(request.args.get('age'))
+    if age < 18:
+        price = 700
+    else:
+        price = 1000
+
+    place = request.args.get('place')
+    if place == 'side_low':
+        price += 100
+        place = 'нижняя боковая'
+    elif place == 'low':
+        price += 100
+        place = 'нижняя'
+    elif place == 'up':
+        price += 0
+        place = 'верхняя'
+    else:
+        price += 0
+        place = 'верхняя боковая'
+
+    if request.args.get('underwear') == 'on':
+        price += 75
+    if request.args.get('luggage') == 'on':
+        price += 250
+    if request.args.get('belay') == 'on':
+        price += 150
+
+    return render_template('lab3/ticket.html', price=price, user=user, age=age, 
+                           place=place, departure=departure, arrival=arrival, date=date, 
+                           underwear=underwear, luggage=luggage, belay=belay)
