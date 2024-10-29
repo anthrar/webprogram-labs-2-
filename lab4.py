c0c3bc250ed04096489ac69426d399a6c09d30b6
Красов Павел <pavel.a.krasov@gmail.com>
2024-10-29 18:14:03 +0700

    Арифметика

--- lab4.py
+++ lab4.py
@@ -22,3 +22,72 @@ def div():
         return render_template('lab4/div.html', error ='На ноль делить нельзя!')
     result = x1/x2
     return render_template('lab4/div.html', x1=x1, x2=x2, result=result)
+
+@lab4.route("/lab4/sum-form")
+def sum_form():
+    return render_template('lab4/sum-form.html')
+
+@lab4.route("/lab4/sum", methods = ['POST'])
+def sum():
+    x1 = request.form.get('x1')
+    x2 = request.form.get('x2')
+    if x1 =='':
+        x1 = '0' 
+    if x2 == '':
+        x2 = '0'
+    x1 = int(x1)
+    x2 = int(x2)
+    result = x1+x2
+    return render_template('lab4/sum.html', x1=x1, x2=x2, result=result)
+
+@lab4.route("/lab4/multi-form")
+def multi_form():
+    return render_template('lab4/multi-form.html')
+
+@lab4.route("/lab4/multi", methods = ['POST'])
+def multi():
+    x1 = request.form.get('x1')
+    x2 = request.form.get('x2')
+    if x1 =='':
+        x1 = '1' 
+    if x2 == '':
+        x2 = '1'
+    x1 = int(x1)
+    x2 = int(x2)
+    result = x1*x2
+    return render_template('lab4/multi.html', x1=x1, x2=x2, result=result)
+
+@lab4.route("/lab4/minus-form")
+def minus_form():
+    return render_template('lab4/minus-form.html')
+
+@lab4.route("/lab4/minus", methods = ['POST'])
+def minus():
+    x1 = request.form.get('x1')
+    x2 = request.form.get('x2')
+    if x1 =='' or x2 == '':
+        return render_template('lab4/minus.html', error ='Оба поля должны быть заполнены!')
+    
+    x1 = int(x1)
+    x2 = int(x2)
+    result = x1-x2
+    return render_template('lab4/minus.html', x1=x1, x2=x2, result=result)
+
+@lab4.route("/lab4/exponen-form")
+def exponen_form():
+    return render_template('lab4/exponen-form.html')
+
+@lab4.route("/lab4/exponen", methods = ['POST'])
+def exponeb():
+    x1 = request.form.get('x1')
+    x2 = request.form.get('x2')
+    if x1 =='' or x2 == '':
+        return render_template('lab4/exponen.html', error ='Оба поля должны быть заполнены!')
+    
+    x1 = int(x1)
+    x2 = int(x2)
+    if x1 == 0 or x2 == 0:
+        return render_template('lab4/exponen.html', error ='Поля не могут быть нулевыми!')
+    result = x1**x2
+    return render_template('lab4/exponen.html', x1=x1, x2=x2, result=result)
+
