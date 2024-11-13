c2021778c06f15c495207bc0082465f5f5a112e1
Красов Павел <pavel.a.krasov@gmail.com>
2024-11-13 17:06:38 +0700

    Регистрация

--- lab5.py
+++ lab5.py
@@ -1,4 +1,5 @@
 from flask import Blueprint, redirect, url_for, render_template, request, session
+import psycopg2
 lab5 = Blueprint('lab5',__name__)
 
 @lab5.route("/lab5/")
@@ -7,3 +8,32 @@ def lab():
     user_name = 'Anonumoys'
     return render_template('lab5/lab5.html', user_name=user_name)
 
+@lab5.route("/lab5/register", methods = ['GET', 'POST'])
+def register():
+    if request.method == 'GET':
+        return render_template('lab5/register.html')
+    
+    login = request.form.get('login')
+    password = request.form.get('password')
+    if not (login or password):
+        return render_template('lab5/register.html', error = 'Заполните поля!')
+    
+    conn = psycopg2.connect(
+        host = '127.0.0.1',
+        database = 'pavel_krasov_knowledge_base',
+        user = 'pavel_krasov_knowledge_base',
+        password = '777'
+    )
+    cur = conn.cursor()
+
+    cur.execute(f"Select login From users Where login='{login}';")
+    if cur.fetchone():
+        cur.close()
+        conn.close()
+        return render_template('lab5/register.html', error = 'Такой пользователь уже существует!')
+    
+    cur.execute(f"Insert into users (login, password) Values ('{login}', '{password}');")
+    conn.commit()
+    cur.close()
+    conn.close()
+    return render_template('lab5/success.html', login=login)
