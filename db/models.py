c207b0c3a27014b255eaf40e4476cc866c5a44cf
Красов Павел <pavel.a.krasov@gmail.com>
2024-12-19 22:20:22 +0700

    Модель базы данных

---
+++ db/models.py
@@ -0,0 +1,18 @@
+from . import db
+from flask_login import UserMixin
+
+class users(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    login = db.Column(db.String(80), unique=True, nullable=False)
+    password = db.Column(db.String(180),  nullable=False)  
+
+class articles(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    login_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    title = db.Column(db.String(80), nullable=False)
+    article_text = db.Column(db.Text, nullable=False)
+    is_favorite = db.Column(db.Boolean)
+    is_public = db.Column(db.Boolean)
+    likes = db.Column(db.Integer)
+
+
