068e2786a24d3359cfc1346eaf20c5854de38252
Красов Павел <pavel.a.krasov@gmail.com>
2024-12-19 22:37:56 +0700

    Регистрация

--- lab8.py
+++ lab8.py
@@ -5,9 +5,38 @@ from werkzeug.security import check_password_hash, generate_password_hash
 import sqlite3
 from os import path
 
+from db import db
+from db.models import users, articles
+from flask_login import login_required, login_user, current_user, logout_user
+
 lab8 = Blueprint('lab8',__name__)
 
 # routes
 @lab8.route("/lab8/")
 def lab():
     return render_template('lab8/lab8.html')
+
+@lab8.route("/lab8/register", methods=['GET', 'POST'])
+def register():
+    if request.method == 'GET':
+        return render_template('lab8/register.html')
+    
+    login = request.form.get('login')
+    password = request.form.get('password')
+
+    login_exists = users.query.filter_by(login=login).first()
+    if login_exists:
+        return render_template('lab8/register.html', error = 'Такой пользователь уже существует!')
+    
+    if login == '':
+        return render_template('lab8/register.html', error = 'имя пользователя не должно быть пустым')
+    if password == '':
+        return render_template('lab8/register.html', error = 'пароль не должен быть пустым')
+
+    password_hash = generate_password_hash(password)
+    user = users(login=login, password=password_hash)   
+    db.session.add(user)    
+    db.session.commit()
+
+    return redirect('/lab8/')
+
